cc829771244d1ae570500399ecabfdd214309ca8
WitchPuff <WitchPuff@githube.com>
2023-05-10 11:58:15 +0800

    modify data generation

--- Data/data_generation/concatenate.py
+++ Data/data_generation/concatenate.py
@@ -2,15 +2,14 @@ import numpy as np
 
 default = ('asap', 'EMOPIA', 'Pianist8', 'POP1K7', 'POP909')
 
-for tag in ('train', 'test', 'valid'):
-    output = None
-    for d in default:
-        # a = np.load(f'Data\output\{d}\midi_{tag}.npy')
-        a = np.load(f'Data\output\{d}\midi_{tag}_split.npy')
-        if output is None:
-            output = a
-        else:
-            output = np.concatenate((output, a), axis=0)
-    # np.save(f'Data\output\midi_{tag}.npy', output)
-    np.save(f'Data\output\midi_{tag}_split.npy', output)
-    print(output.shape)
+for flag in ('', '_split'):
+    for tag in ('train', 'test', 'valid'):
+        output = None
+        for d in default:
+            a = np.load(f'Data\output\{d}\midi_{tag}{flag}.npy')
+            if output is None:
+                output = a
+            else:
+                output = np.concatenate((output, a), axis=0)
+        np.save(f'Data\output\midi_{tag}{flag}.npy', output)
+        print(output.shape)
