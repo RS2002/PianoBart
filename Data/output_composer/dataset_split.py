3510d68ea5c51d8964b25ea60623c0048e25b6e0
RS2002 <448708588@qq.com>
2023-08-13 19:15:39 +0800

    change the proportion of Pianst8

--- Data/output_composer/dataset_split.py
+++ Data/output_composer/dataset_split.py
@@ -25,8 +25,13 @@ def split(data_list,label_list,prob_list):
         previous=temp
 
 
+# split(
+#     ["./asap/asap_train.npy","./asap/asap_valid.npy","./asap/asap_test.npy"],
+#     ["./asap/asap_train_comans.npy","./asap/asap_valid_comans.npy","./asap/asap_test_comans.npy"],
+#     [0.8,0.1,0.1]
+# )
 split(
-    ["./asap/asap_train.npy","./asap/asap_valid.npy","./asap/asap_test.npy"],
-    ["./asap/asap_train_comans.npy","./asap/asap_valid_comans.npy","./asap/asap_test_comans.npy"],
+    ["./Pianist8/Pianist8_train.npy","./Pianist8/Pianist8_valid.npy","./Pianist8/Pianist8_test.npy"],
+    ["./Pianist8/Pianist8_train_comans.npy","./Pianist8/Pianist8_valid_comans.npy","./Pianist8/Pianist8_test_comans.npy"],
     [0.8,0.1,0.1]
 )
