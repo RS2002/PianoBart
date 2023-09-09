476a529226b3a74546c7755d54bc629aace2f85c
Zeng-WCh <lewis.zeng02@gmail.com>
2023-09-09 10:45:02 +0800

    Bug fixed

--- finetune_generation.py
+++ finetune_generation.py
@@ -246,7 +246,7 @@ class GenerationTrainer:
                         valid_loss, train_loss, is_best, filename):
         state = {
             'epoch': epoch + 1,
-            'state_dict': self.model.module.state_dict(),
+            'state_dict': self.model.state_dict(),
             'valid_acc': valid_acc,
             'valid_loss': valid_loss,
             'train_loss': train_loss,
