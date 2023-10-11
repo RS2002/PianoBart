d433e0504129f5f541e53ab907ddcfe9f737395b
RS2002 <448708588@qq.com>
2023-10-11 16:39:21 +0800

    delete garbage files

--- Data/output_composer/test.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-a=np.load("./asap/asap_test.npy")
-for i in range(1024):
-    print(a[2,i,0])
--- Data/output_pretrain/test.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-a=np.load("./POP909/POP909_train_split.npy")
-for i in range(1024):
-    print(a[110][i])
